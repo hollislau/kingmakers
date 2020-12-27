260e64434a736fecd6cd835aefdfec436ce1c114
Hollis Lau <hollislau@hotmail.com>
2020-12-26 22:21:47 -0800

    * Add Amplify authentication flow

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import { AmplifySignOut } from '@aws-amplify/ui-react'
+import { useState, useEffect } from 'react'
+import {
+  AmplifyAuthenticator,
+  AmplifySignIn,
+  AmplifyGreetings,
+} from '@aws-amplify/ui-react'
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components'
 
 import classes from './App.module.scss'
 
 const App = () => {
-  return (
+  const [authState, setAuthState] = useState()
+  const [user, setUser] = useState()
+
+  useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState)
+      setUser(authData)
+    })
+  }, [])
+
+  return authState === AuthState.SignedIn && user ? (
     <div className={classes.container}>
       <header>
-        <AmplifySignOut />
+        <AmplifyGreetings username={user.attributes?.given_name} />
       </header>
+      <main>
+        <h1>Hello</h1>
+      </main>
     </div>
+  ) : (
+    <AmplifyAuthenticator usernameAlias="email">
+      <AmplifySignIn
+        slot="sign-in"
+        usernameAlias="email"
+        headerText="Sign in to Kingmakers"
+        hideSignUp={true}
+      />
+    </AmplifyAuthenticator>
   )
 }
 
